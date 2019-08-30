e768dbbc3bbb180834fab749d2011f1479be272c
DaiYuXing <daiyu_zz@163.com>
2019-08-30 15:01:36 +0800

    文章目录

--- article/views.py
+++ article/views.py
@@ -7,27 +7,44 @@ from .models import ArticlePost
 from .forms import ArticlePostForm
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 # Create your views here.
 
 # 视图函数
 def article_list(request):
-    # 根据GET请求中查询条件，返回不同排序的对象数组
-    if request.GET.get('order') == 'total_views':
-        article_list = ArticlePost.objects.all().order_by('-total_views')
-        order = 'total_views'
+    search = request.GET.get('search')
+    order = request.GET.get('order')
+    # 用户搜索逻辑
+    if search:
+        if order == 'total_views':
+            # 利用Q对象进行联合搜索
+            article_list = ArticlePost.objects.filter(
+                Q(title__icontains=search) |
+                Q(body__icontains=search)
+            ).order_by('-total_views')
+        else:
+            article_list = ArticlePost.objects.filter(
+                Q(title__icontains=search) |
+                Q(body__icontains=search)
+            )
     else:
-        article_list = ArticlePost.objects.all()
-        order = 'normal'
-    # 每页显示1篇文章
+        # 将search参数置空
+        search = ''
+        if order == 'total_views':
+            article_list = ArticlePost.objects.all().order_by('-total_views')
+        else:
+            article_list = ArticlePost.objects.all()
+
+    # 每页显示3篇文章
     paginator = Paginator(article_list, 3)
     # 获取url中的页码
     page = request.GET.get('page')
     # 将导航对象相应的页码内容返回给articles
     articles = paginator.get_page(page)
     # 需要传递给模板的对象
-    context = {'articles': articles, 'order': order}
+    context = {'articles': articles, 'order': order, 'search': search}
     # render函数：载入模板，并返回context对象
     return render(request, 'article/list.html', context)
 
@@ -39,15 +56,18 @@ def article_detail(request, id):
     article.total_views += 1
     article.save(update_fields=['total_views'])
     # 将markdown语法渲染成html样式
-    article.body = markdown.markdown(article.body,
-                                     extensions=[
-                                         # 包含 缩写、表格等常用扩展
-                                         'markdown.extensions.extra',
-                                         # 语法高亮扩展
-                                         'markdown.extensions.codehilite',
-                                     ])
-    # 需要传递给模板对象
-    context = {'article': article}
+    md = markdown.Markdown(
+        extensions=[
+            # 包含 缩写、表格等常用扩展
+            'markdown.extensions.extra',
+            # 语法高亮扩展
+            'markdown.extensions.codehilite',
+            # 目录拓展
+            'markdown.extensions.toc',
+        ])
+    article.body = md.convert(article.body)
+    # 需要传递给模板对象,新增md.toc对象
+    context = {'article': article, 'toc': md.toc}
     return render(request, 'article/detail.html', context)
 
 
